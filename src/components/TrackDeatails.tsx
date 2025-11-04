5f1b3b81a3b9b78820724efdf2392c80e1716b17
Andriy <andygrigs88@gmail.com>
2025-11-04 20:49:22 +0100

    implement two new files

---
+++ src/components/TrackDeatails.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrackDeatails = () => {
+  return (
+    <div>TrackDeatails</div>
+  )
+}
+
+export default TrackDeatails
---
+++ src/components/TrackLIst.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrackLIst = () => {
+  return (
+    <div>TrackLIst</div>
+  )
+}
+
+export default TrackLIst
